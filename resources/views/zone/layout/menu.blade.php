d6d8393bb6e16b1b8a9a161cce93669088465c6e
MyintMyatKaungPHP <myintmyatkaung.info@gmail.com>
2018-08-29 13:27:20 +0630

    index page and tour packages page design ok

---
+++ resources/views/zone/layout/menu.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Myint Myat Kaung
+ * Date: 29-Aug-18
+ * Time: 10:49 AM
+ */
---
+++ resources/views/zone/package_detail.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Myint Myat Kaung
+ * Date: 29-Aug-18
+ * Time: 1:26 PM
+ */
